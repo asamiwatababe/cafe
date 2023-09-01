2896cd0020767ad69212286f975ff946876db4a6
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-09-01 22:03:12 +0900

    likeボタンの装飾

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,7 @@ class Tweet < ApplicationRecord
     belongs_to :user
     has_many :comments
     has_one_attached :image
-    has_many :likes
+    has_many :likes, dependent: :destroy
 
     def liked_by?(user)
         likes.where(user_id: user.id).exists?
