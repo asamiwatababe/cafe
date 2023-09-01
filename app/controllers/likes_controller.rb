d528cbc69864acb561645a013a7dbee6328d5561
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-09-01 14:05:38 +0900

    作成したファイルが読み込まれるように実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,23 @@
 class LikesController < ApplicationController
+    before_action :set_tweet
+
     def create
         like = current_user.likes.build(tweet_id: params[:tweet_id])
         like.save
-        redirect_to root_path
+        respond_to do |format|
+          format.js
+        end
     end
     
     def destroy
         like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
         like.destroy
-        redirect_to root_path
+        respond_to do |format|
+            format.js
+        end
+    end
+
+    def set_tweet
+        @tweet = Tweet.find(params[:tweet_id])
     end
 end
