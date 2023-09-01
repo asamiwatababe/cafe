b7cdf903caec8e3b2c1d4b99d860749334d2b7db
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-09-01 11:38:40 +0900

    検索機能追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,12 @@ class Tweet < ApplicationRecord
     belongs_to :user
     has_many :comments
     has_one_attached :image
+
+    def self.search(search)
+        if search != ""
+          Tweet.where('text LIKE(?)', "%#{search}%")
+        else
+          Tweet.all
+        end
+    end
 end
