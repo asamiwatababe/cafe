641ed4f542caf870b45ef172a61c593e01dc49bd
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-09-02 13:07:30 +0900

    ログインしているユーザーのみの設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
     before_action :set_tweet
+    before_action :authenticate_user!
 
     def create
         like = current_user.likes.build(tweet_id: params[:tweet_id])
