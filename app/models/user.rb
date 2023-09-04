66a53b5f59f9c90189cbad8b59971c8903b94257
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-09-04 20:18:41 +0900

    コマンドミス修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tweets, dedependent: :destroy
+  has_many :tweets, dependent: :destroy
   has_many :comments
   has_many :likes, dependent: :destroy
   
